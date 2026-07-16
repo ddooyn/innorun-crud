55183275743ea805f18af1c9bd83049354c93141
Sooyoung Yoon <ayoooo240@gmail.com>
2026-07-16 13:49:30 +0900

    chore(movie): 초기 설정

---
+++ 260716 movie/src/main/java/org/practice/springcrudmovie/SpringCrudMovieApplication.java
@@ -0,0 +1,13 @@
+package org.practice.springcrudmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrudMovieApplication.class, args);
+    }
+
+}
---
+++ 260716 movie/src/test/java/org/practice/springcrudmovie/SpringCrudMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practice.springcrudmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudMovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
