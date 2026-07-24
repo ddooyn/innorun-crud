2d1a9107bad88a191eff806a4c6ec326d384fda5
Sooyoung Yoon <ayoooo240@gmail.com>
2026-07-24 12:53:03 +0900

    chore: 초기 설정

---
+++ argumentresolver/src/test/java/com/innorun/argumentresolver/ArgumentresolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innorun.argumentresolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgumentresolverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
